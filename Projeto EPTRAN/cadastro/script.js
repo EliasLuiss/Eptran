9f737828e094eace7fb98ca670955b796c7974eb
EliasLuiss <elias71bier@gmail.com>
2024-07-04 12:50:03 -0300

    eptran

--- Projeto EPTRAN/cadastro/script.js
+++ Projeto EPTRAN/cadastro/script.js
@@ -31,3 +31,61 @@ function confereSenha() {
       });    
   });
 })();
+
+const form = document.getElementById('form');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const cep = document.getElementById('cep');
+const rua = document.getElementById('rua');
+const bairro = document.getElementById('bairro');
+const cidade = document.getElementById('cidade');
+const estado = document.getElementById('estado');
+const escola = document.getElementById('escola');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error')
+};
+
+const validadeInputs = ()=> { 
+  const firstnameValue = firstname.value.trim(); 
+  const lastnameValue = lastname.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const confirmPasswordValue = confirmPassword.value.trim();
+  const cepValue = cep.value.trim();
+  const ruaValue = rua.value.trim();
+  const bairroValue = bairro.value.trim();
+  const cidadeValue = cidade.value.trim();
+  const estadoValue = estado.value.trim();
+  const escolaValue = escola.value.trim();
+
+  if(firstnameValue === '') {
+      setError(firstname, 'Nome é requisitado');
+  } else {
+    setSuccess(firstname);
+  }
+
+};
